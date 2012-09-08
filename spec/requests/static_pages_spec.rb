c71edf49f46e41e6c68ec3262d18b654db66d705
Andrew January <andrewjanuary@gmail.com>
2012-09-08 14:17:57 +0100

    Change name of tests to refer to heading rather than title

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,21 +2,21 @@ require 'spec_helper'
 
 describe 'StaticPages' do
   describe 'Home Page' do
-    it "should have the title 'Sample App'" do
+    it "should have the heading 'Sample App'" do
       visit '/static_pages/home'
       page.should have_selector('h1', :text => 'Sample App')
     end
   end
 
   describe 'Help Page' do
-    it "should have the title 'Help'" do
+    it "should have the heading 'Help'" do
       visit '/static_pages/help'
       page.should have_selector('h1', :text => 'Help')
     end
   end
 
   describe 'About Page' do
-    it "should have the title 'About Us'" do
+    it "should have the heading 'About Us'" do
       visit '/static_pages/about'
       page.should have_selector('h1', :text => 'About Us')
     end
